d8ad06311b03c492698910c3befce6ab5906332f
Ilya Posov <iposov@gmail.com>
2021-09-18 17:47:36 +0300

    add tests

--- 21fall/prog_basics/test-system/intro_tasks.py
+++ 21fall/prog_basics/test-system/intro_tasks.py
@@ -106,3 +106,69 @@ def poly(a, b, c):
 
 poly_coefs = [-100, -99, -239, 100, 99, 239] + list(range(-3, 4))
 create_tests(poly, [[a, b, c] for a in poly_coefs for b in poly_coefs for c in poly_coefs])
+
+
+def number_to_string(n):
+    ones = ["ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"]
+    ones_fem = [ones[0], "одна", *ones[2:]]
+    teens = ["десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать",
+             "семнадцать", "восемнадцать", "девятнадцать"]
+    tens = ["ноль", "десять", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят",
+            "девяносто"]
+    hundreds = ["ноль", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот",
+                "девятьсот"]
+    words_million = ["миллион", "миллиона", "миллионов"]
+    words_thousand = ["тысяча", "тысячи", "тысяч"]
+
+    def triple(t, ones):
+        a = t // 100
+        b = t // 10 % 10
+        c = t % 10
+
+        res = []
+        if a > 0:
+            res.append(hundreds[a])
+        if b > 1:
+            res.append(tens[b])
+        if b == 1:
+            res.append(teens[c])
+        else:
+            res.append(ones[c])
+
+        return res
+
+    def word(n, words):
+        last = n % 10
+        pre_last = n // 10 % 10
+
+        if pre_last == 1 or last == 0 or 5 <= last <= 9:
+            return words[2]
+        if last == 1:
+            return words[0]
+        return words[1]
+
+    t1 = n // 1_000_000
+    t2 = n // 1_000 % 1_000
+    t3 = n % 1_000
+
+    res = []
+    if t1 > 0:
+        res += triple(t1, ones)
+        res.append(word(t1, words_million))
+    if t2 > 0:
+        res += triple(t2, ones_fem)
+        res.append(word(t2, words_thousand))
+    if t3 > 0:
+        res += triple(t3, ones)
+
+    return " ".join(res)
+
+
+nums = [[x] for x in range(1, 2001)]
+nums += [[x] for x in range(1, 1_000_000_000, 479909)]  # 40000th prime
+triplets = list(range(0, 30)) + list(range(100, 130)) + [111 * x for x in range(1, 10)]
+nums += [[1_000_000 * t1 + 1_000 * t2 + t3] for t1 in triplets for t2 in triplets for t3 in triplets if t1 != 0 or t2 != 0 or t3 != 0]
+
+# create_tests(number_to_string, nums)
+create_tests(number_to_string, [[x] for x in range(0, 1000)], filename="tests/number_to_string_short.json.gz")
+
