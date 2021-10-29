eb99445c90f4790fe56384fb6a4b2efd7b2dc26b
Ilya Posov <iposov@gmail.com>
2021-10-29 15:47:11 +0300

    add all_tests

---
+++ 21fall/prog_basics/test-system/strings_tasks.py
@@ -0,0 +1,118 @@
+from testsystem import write_test_suites, create_tests, test, TestSuite
+
+
+def palindrome_test(function, test_environment):
+    def is_palindrome_strict(text, strict):
+        if not strict:
+            text = text.replace(" ", "").lower()
+        return text[::-1] == text
+
+    use_strict = test_environment.data
+
+    symbols = ('a', 'b', ' ', 'A', 'B')
+    word = []
+    max_level = 7
+
+    def search(level):
+        text = ''.join(word)
+        if use_strict:
+            function.call(text, True)
+            test_environment.assert_result_equals(is_palindrome_strict(text, True))
+            function.call(text, False)
+            test_environment.assert_result_equals(is_palindrome_strict(text, False))
+        else:
+            function.call(text)
+            test_environment.assert_result_equals(is_palindrome_strict(text, True))
+        if level == max_level:
+            return
+        word.append(None)
+        for symbol in symbols:
+            word[-1] = symbol
+            search(level + 1)
+        del word[-1]
+
+    search(0)
+
+
+write_test_suites("tests/palindrome.json.gz", TestSuite(palindrome_test, False))
+write_test_suites("tests/palindrome_strict.json.gz", TestSuite(palindrome_test, True))
+
+
+def _is_poly(text):
+    return text == text[::-1]
+
+
+def _is_poly_strict(text, strict):
+    if not strict:
+        # text = text.replace(" ", "").lower()
+        text = text.replace(" ", "").lower()
+    return text == text[::-1]
+
+
+test(_is_poly, "tests/palindrome.json.gz")
+test(_is_poly_strict, "tests/palindrome_strict.json.gz")
+
+
+def symbol_pairs(text):
+    return text[::2] == text[1::2]
+
+
+create_tests(symbol_pairs, [
+    [""], ["a"], ["A"], ["aabb"], ["aaBB"], ["aaB"], ["aBB"], ["aabbccdd"], ["aabbccdde"], ["aabbccde"]
+])
+
+
+def lower_upper(text):
+    t1 = text[::2]
+    t2 = text[1::2]
+    return t1.islower() and t2.isupper() or t1.isupper() and t2.islower()
+
+
+create_tests(lower_upper, [
+    [""], ["a"], ["A"], ["aAbB"], ["aAbBc"], ["aAbBC"], ["aaAbBc"], ["aA" * 20], ["aB" * 20 + "a"]
+])
+
+
+def exchange_symbols(text, c1, c2):
+    rules = str.maketrans(c1 + c2, c2 + c1)
+    return text.translate(rules)
+
+
+string_with_all_symbols = ''.join(chr(i) for i in range(32, 128))
+create_tests(exchange_symbols, [
+    ["", "0", "1"],
+    ["01", "0", "1"],
+    ["aaaabbbbb", "a", "b"],
+    ["aaaabbbbb", "x", "y"],
+    ["aaaxabbybbb", "a", "b"],
+    ["hello world", "l", "d"]
+] + [[string_with_all_symbols * 2, c1, c2] for c1 in "a9B~!@#$%^&*()" for c2 in "a9B~!@#$%^&*()-+" if c1 != c2])
+
+create_tests(lambda text: exchange_symbols(text, '0', '1'), [
+    [""],
+    ["01010"],
+    ["01001asdf00020123"],
+    ["asdlkjaslkdfj"],
+] + [[bin(x).replace("0b", "")] for x in range(128)] +
+             [[bin(x).replace("0b", "") + "abc"] for x in range(128)], filename="tests/exchange_01.json.gz")
+
+
+def dragon_curve(n):
+    s = "R"
+    for i in range(n):
+        s = s + "R" + exchange_symbols(s, "R", "L")[::-1]
+    return s
+
+
+create_tests(dragon_curve, [[n] for n in range(5)])
+
+
+def dragon_curve_(n):
+    s = "R"
+    for i in range(n):
+        s = s + "R" + exchange_symbols(s, "R", "L")[::-1]
+    # if n == 3:
+    #     return s[:4] + 'x' + s[5:]
+    return s
+
+test(dragon_curve_, "tests/dragon_curve.json.gz")
--- 21fall/prog_basics/test-system/testsystem.py
+++ 21fall/prog_basics/test-system/testsystem.py
@@ -9,64 +9,11 @@ colorama_init()
 
 
 # TODO add number of tests that are allowed to fail
-
-
-class Message:
-    def __init__(self, message, detailed_message=None, args=None, kwargs=None, input_stream=None, expected_value=None,
-                 actual_value=None, function_name=None):
-        self.message = message
-        self.detailed_message = detailed_message
-        self.args = args
-        self.kwargs = kwargs
-        self.input_stream = input_stream
-        self.expected_value = expected_value
-        self.actual_value = actual_value
-        self.function_name = function_name
-
-    def print(self):
-        # def has_several_lines(text):
-        #     return '\n' in text.strip()
-
-        print(f"{Fore.RED}Ошибка при проверке {self.function_name}: {self.message}{Fore.RESET}")
-        if self.detailed_message is not None:
-            print(self.detailed_message)
-        if self.args is not None or self.kwargs is not None:
-            joined_args = ''
-            if self.args is not None:
-                joined_args += ', '.join(str(arg) for arg in self.args)
-            if self.kwargs is not None:
-                joined_args += ', '.join(f'{key}={value}' for key, value in self.kwargs.items())
-            print(f"Вызов {self.function_name}({joined_args})")
-        if self.input_stream is not None:
-            print("Данные для входного потока:")
-            print(f"{Back.LIGHTYELLOW_EX}{self.input_stream}{Back.RESET}")
-        if self.expected_value is not None or self.actual_value is not None:
-            # '·' \u00B7 for spaces
-            expected_text = str(self.expected_value)  # .replace(' ', '\u00B7')
-            actual_text = str(self.actual_value)  # .replace(' ', '\u00B7')
-            different_symbol = -1
-            min_length = min(len(actual_text), len(expected_text))
-            for i in range(min_length):
-                if expected_text[i] != actual_text[i]:
-                    different_symbol = i
-                    break
-            ending = ''
-            if different_symbol == -1 and len(actual_text) != len(expected_text):
-                different_symbol = min_length
-                ending = '\u23F9'
-            if actual_text[different_symbol:].isspace() or expected_text[different_symbol:].isspace():
-                ending = '\u23F9'
-
-            print("Получено:")
-            print(
-                f'{Back.LIGHTGREEN_EX}{actual_text[:different_symbol]}{Back.LIGHTRED_EX}{actual_text[different_symbol:]}{ending}{Style.RESET_ALL}')
-            print("Ожидалось:")
-            print(
-                f'{Back.LIGHTGREEN_EX}{expected_text[:different_symbol]}{Back.LIGHTCYAN_EX}{expected_text[different_symbol:]}{ending}{Style.RESET_ALL}')
-
+# TODO allow checker function to be a list of functions
 
 class FunctionWrapper:
-    def __init__(self, function):
+    def __init__(self, function, additional_parameters):
+        self.name = function.__name__
         self.function = function
         self.stdout = ""
         # self.stderr = ""
@@ -74,11 +21,14 @@ class FunctionWrapper:
         self.args = None
         self.kwargs = None
         self.stdin = None
+        self.result = None
+        # TODO self.check_output_is_always_empty, self.check_results_is_always_None
 
     def call(self, *args, stdin=None, **kwargs):
         self.args = args
         self.kwargs = kwargs
         self.stdin = stdin
+        self.result = None
         if stdin is not None:
             sys.stdin = io.StringIO(stdin)
         sys.stdout = io.StringIO()
@@ -86,7 +36,8 @@ class FunctionWrapper:
         # self.exception = None
 
         try:
-            return self.function(*args, **kwargs)
+            self.result = self.function(*args, **kwargs)
+            return self.result
         # except BaseException as e:
         #     self.exception = e
         finally:
@@ -95,17 +46,17 @@ class FunctionWrapper:
             sys.stdin = sys.__stdin__
             sys.stdout = sys.__stdout__
 
-    def message(self, message, detailed_message=None, expected_value=None, actual_value=None):
-        return Message(
-            message,
-            detailed_message,
-            args=self.args,
-            kwargs=self.kwargs,
-            input_stream=self.stdin,
-            expected_value=expected_value,
-            actual_value=actual_value,
-            function_name=self.function.__name__
-        )
+    def print_call_information(self):
+        if self.args is not None or self.kwargs is not None:
+            joined_args = ''
+            if self.args is not None:
+                joined_args += ','.join(str(arg) for arg in self.args)
+            if self.kwargs is not None:
+                joined_args += ','.join(f'{key}={value}' for key, value in self.kwargs.items())
+            print(f"Вызов {self.function.__name__}({joined_args})")
+        if self.stdin is not None:
+            print("Данные для входного потока:")
+            print(f"{Back.LIGHTYELLOW_EX}{self.stdin}{Back.RESET}")
 
 
 def compare_return_value_with_expected(function, test_environment):
@@ -113,27 +64,19 @@ def compare_return_value_with_expected(function, test_environment):
     for test_case in test_cases:
         *args, correct_answer = test_case
         student_answer = function.call(*args)
-        if correct_answer != student_answer:
-            test_environment.Message("Результат вызова функции не совпадает с правильным",
-                                     args=args,
-                                     expected_value=correct_answer,
-                                     actual_value=student_answer
-                                     ).print()
-            return False
-    return True
+        test_environment.assert_result_equals(correct_answer)
 
 
 class TestSuite:
-    Message = Message
     CheckerFunctions = (compare_return_value_with_expected,)
 
     def __init__(self, checker_function, data=None):
         self.checker_function = checker_function
         self.data = data
 
-    def run(self, function):
-        wrapper = FunctionWrapper(function)
-        return self.checker_function(wrapper, self)
+    def run(self, function, additional_wrapper_parameters, additional_environment_parameters):
+        wrapper = FunctionWrapper(function, additional_wrapper_parameters)
+        self.checker_function(wrapper, TestEnvironment(wrapper, self.data, additional_environment_parameters))
 
     def serialize(self):
         result = {'checker': self.checker_function.__name__}
@@ -159,6 +102,75 @@ class TestSuite:
         return TestSuite(checker_function, serialized_object.get('data'))
 
 
+class TestEnvironment:
+    def __init__(self, wrapper, data, additional_parameters):
+        self.wrapper = wrapper
+        self.data = data
+
+        def get_param(name, default):
+            if additional_parameters:
+                return additional_parameters.get(name, default)
+            return default
+
+        self.replace_spaces = get_param('replace', False)
+
+    def _print_message(self, message=None, detailed_message=None, print_call=True):
+        if message is not None:
+            print(f"{Fore.RED}Ошибка при проверке {self.wrapper.name}: {message}{Fore.RESET}")
+        else:
+            print(f"{Fore.RED}Ошибка при проверке {self.wrapper.name}{Fore.RESET}")
+        if detailed_message is not None:
+            print(detailed_message)
+        if print_call:
+            self.wrapper.print_call_information()
+
+    def fail(self, message, detailed_message, print_call=True):
+        self._print_message(message, detailed_message)
+        raise AssertionError()
+
+    def assert_equals(self, expected_value, actual_value, message=None, detailed_message=None, print_call=True):
+        if expected_value == actual_value:
+            return
+
+        self._print_message(message, detailed_message, print_call)
+
+        expected_text = str(expected_value)
+        actual_text = str(actual_value)
+        if self.replace_spaces is not False:
+            replace_symbol = '\u00B7' if self.replace_spaces is True else self.replace_spaces
+            expected_text = expected_text.replace(' ', replace_symbol)
+            actual_text = actual_text.replace(' ', replace_symbol)
+        different_symbol = -1
+        min_length = min(len(actual_text), len(expected_text))
+        for i in range(min_length):
+            if expected_text[i] != actual_text[i]:
+                different_symbol = i
+                break
+        ending = ''
+        if different_symbol == -1 and len(actual_text) != len(expected_text):
+            different_symbol = min_length
+            ending = '\u23F9'
+        if actual_text[different_symbol:].isspace() or expected_text[different_symbol:].isspace():
+            ending = '\u23F9'
+
+        print("Получено:")
+        print(
+            f'{Back.LIGHTGREEN_EX}{actual_text[:different_symbol]}{Back.LIGHTRED_EX}{actual_text[different_symbol:]}{ending}{Style.RESET_ALL}')
+        print("Ожидалось:")
+        print(
+            f'{Back.LIGHTGREEN_EX}{expected_text[:different_symbol]}{Back.LIGHTCYAN_EX}{expected_text[different_symbol:]}{ending}{Style.RESET_ALL}')
+
+        raise AssertionError()
+
+    def assert_result_equals(self, expected_result):
+        self.assert_equals(expected_value=expected_result, actual_value=self.wrapper.result,
+                           message="Результат функции не совпадает с ожидаемым")
+
+    def assert_output_equals(self, expected_output):
+        self.assert_equals(expected_value=expected_output, actual_value=self.wrapper.stdout,
+                           message="Результат функции не совпадает с ожидаемым")
+
+
 def deserialize_test_suites(serialized_object):
     if len(serialized_object) == 0:
         return []
@@ -175,7 +187,7 @@ def serialize_test_suites(test_suites):
 
 # public functions
 
-def test(function, filename):
+def test(function, filename, replace_spaces=False):
     if filename[-3:] == '.gz':
         f = gzip.open(filename, "rb")
     else:
@@ -187,10 +199,11 @@ def test(function, filename):
     test_suites = deserialize_test_suites(test_suites_json)
 
     all_tests_passed = True
-    for test_suite in test_suites:
-        if not test_suite.run(function):
-            all_tests_passed = False
-            break
+    try:
+        for test_suite in test_suites:
+            test_suite.run(function, {}, {"replace_spaces": replace_spaces})
+    except AssertionError:
+        all_tests_passed = False
 
     if all_tests_passed:
         print(f"{Fore.GREEN}{function.__name__}: Все тесты пройдены{Fore.RESET}")
