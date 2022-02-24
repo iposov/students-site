cd5c93f56b3b9ac0becb04736c4328610d48df34
Ilya Posov <iposov@gmail.com>
2022-02-24 13:11:31 +0300

    add lectures

---
+++ 22spring/prog_tech/src/posov/Book.java
@@ -0,0 +1,24 @@
+package posov;
+
+public class Book { // в файле Book.java
+
+    // статические элементы класса
+    static int numBooks; // сколько всего книг в библиотеке
+    static void addBook() {
+        Book.numBooks += 1;
+    }
+
+    // элементы класса
+    // поля, хранят данные
+    String title;
+    String author;
+    int year;
+
+    // методы, совершают действия
+    // [тип результата] [имя функции]([аргументы]) {
+    // }
+    // this — аналог self в Python, только в отличие от Python его не нужно писать явно
+    void print() {
+        System.out.println(this.author + ", " + this.title + " (" + this.year + ")");
+    }
+}
