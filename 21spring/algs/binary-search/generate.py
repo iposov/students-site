6adc6797ea08b114d2890e0167ff1bda489c8e37
Ilya Posov <iposov@gmail.com>
2022-03-21 06:59:11 +0300

    add algs

--- 21spring/algs/binary-search/generate.py
+++ 21spring/algs/binary-search/generate.py
@@ -1,7 +1,6 @@
 from random import randint, choice, sample
 
 with open("5.in", "w") as fin, open("5.out", "w") as fout:
-
     N = 1000000
     K = 1000000
     a = sample(range(2000000000), N)
@@ -29,4 +28,3 @@ with open("5.in", "w") as fin, open("5.out", "w") as fout:
             else:
                 print(randint(a[ind] + 1, a[ind + 1] - 1), file=fin)
                 print(-1, file=fout)
-
